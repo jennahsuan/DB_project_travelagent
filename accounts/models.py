d8c176d927b336f37109585ba507a57780127c88
jennahsuan <b07610008@ntu.edu.tw>
2022-12-02 22:39:16 +0800

    models update

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
+import datetime
+
 
 # Create your models here.
+
+class GenderOptions(models.TextChoices):
+    FEMALE = 'Female'
+    MALE = 'Male'
+    UNSURE = 'Unsure'
+
+class NationOptions(models.TextChoices):
+    TAIWAN = 'Taiwan'
+    JAPAN = 'Japan'
+    KOREA = 'Korea'
+    USA = 'USA'
+
+class Member(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    # name = models.CharField(max_length=200, null=False, blank=False, unique=False)
+    phone = PhoneNumberField(null=False, blank=False, unique=True)
+    gender = models.CharField(max_length=1, choices=GenderOptions.choices)
+    birthday = models.DateField(default=datetime.date.today)
+    nation = models.CharField(max_length=1, choices=NationOptions.choices)
+    register_Date = models.DateField(default=datetime.date.today)
+    def __str__(self):
+        return str(self.user)
+
+class Guide(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    # name = models.CharField(max_length=200, null=False, blank=False, unique=False)
+    phone = PhoneNumberField(unique=True)
+    birthday = models.DateField(default=datetime.date.today)
+    language = models.CharField(max_length=200)
+    seniority = models.IntegerField(default=0)
+    def __str__(self):
+        return str(self.user)
+
+
--- travelloapp/models.py
+++ travelloapp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from itinerary.models import *
+from accounts.models import Guide
 
 # Create your models here.
 class Destination(models.Model):
@@ -7,3 +9,22 @@ class Destination(models.Model):
     desc = models.TextField()
     price = models.IntegerField()
     offer = models.BooleanField(default=False)
+
+'''Above is source code'''
+
+class Tour(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    itinerary_plan = models.ForeignKey(Itinerary, on_delete=models.CASCADE, null = False)
+    bus = models.ForeignKey(Bus, on_delete=models.SET_NULL, null = True, blank = True)
+    startDate = models.DateField(auto_now_add=False,  blank=True, default=date.today) 
+    endDate = models.DateField(auto_now=False, blank=True, default=date.today) 
+    price = models.IntegerField()
+    guide = models.ForeignKey(Guide, on_delete=models.SET_NULL, null = True, blank = True)
+    field = models.CharField(max_length=100, null=True, blank=True)
+    total_tourist = models.IntegerField()
+    max_tourist = models.IntegerField()
+    
+    # TODO
+    def update_total_tourist():
+        return
